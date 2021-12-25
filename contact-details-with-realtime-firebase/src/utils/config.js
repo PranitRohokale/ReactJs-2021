01684975d295bced897642def17ef031d10c71aa
Pranit Rohokale <72683329+PranitRohokale@users.noreply.github.com>
2021-12-25 13:57:50 +0530

    Update config.js

--- contact-details-with-realtime-firebase/src/utils/config.js
+++ contact-details-with-realtime-firebase/src/utils/config.js
@@ -1,12 +1,12 @@
 //TODO: add firebase configuration and export it
 export const firebaseConfig = {
-  apiKey: "AIzaSyDKSP9-NU1x0D-VZDclwtFtKycOQMwf0d4",
-  authDomain: "mygitapp-9433e.firebaseapp.com",
-  databaseURL: "https://mygitapp-9433e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mygitapp-9433e",
-  storageBucket: "mygitapp-9433e.appspot.com",
-  messagingSenderId: "324308520452",
-  appId: "1:324308520452:web:ddab2f53f49dca32e92f51",
+  apiKey: "ur key",
+  authDomain: ".firebaseapp.com",
+  databaseURL: "https://-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ur project id",
+  storageBucket: "****.appspot.com",
+  messagingSenderId: "id",
+  appId: "****",
   measurementId: "${config.measurementId}"
 };
 //image configuration
